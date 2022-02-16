88c406f3284e178f1abbf7addf363f8b93daf743
sarvesh <sarveshkumar452000@gmail.com>
2022-02-16 12:15:16 -0800

    Welcome to EmployeeComputation1

---
+++ EmployeeComputation1/EmployeeWage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComputation1
+{
+    internal class EmployeeWage
+    {
+        const int IS_PRESENT = 1;
+        const int IS_ABSENT = 0;
+
+        public void EmployeeAttendance()
+        {
+
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 2);
+
+            if (EmpCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            Console.ReadLine();
+        }
+    
+    }
+}
---
+++ EmployeeComputation1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComputation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation 1");
+            EmployeeWage employee = new EmployeeWage();
+            employee.EmployeeAttendance();
+            Console.ReadLine();
+        }
+    }
+}
